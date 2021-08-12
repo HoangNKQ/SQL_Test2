babaa8e1a100446b2f7ae1ae76250a2130de1268
HoagOWL <nkqhoangtu@gmail.com>
2021-08-12 19:54:52 +0700

    Update Test1.sql

--- Test1.sql
+++ Test1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE countries (
 	country_id CHAR(2) PRIMARY KEY,
 	country_name VARCHAR(40),
-	region_id INT
+	region_id numeric
 )
 
-REFERENCE: PART1 - Chapter 2 - 2.3. Creating a New Table 
+REFERENCE: PARTII - Chapter 5 - 5.1. Table basics 
