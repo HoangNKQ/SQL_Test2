26bdff74b6f6a7adf43a286264bfef91cdbad991
HoagOWL <nkqhoangtu@gmail.com>
2021-08-12 19:46:58 +0700

    New Commit

--- Test1.sql
+++ Test1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE countries (
-	country_id	VARCHAR(5),
-	country_name VARCHAR(50),
-	region_id VARCHAR(5)
+	country_id CHAR(2) PRIMARY KEY,
+	country_name VARCHAR(40),
+	region_id INT
 )
 
-REFERENCE: PART1/Chapter 2/2.3. Creating a New Table 
+REFERENCE: PART1 - Chapter 2 - 2.3. Creating a New Table 
---
+++ Test3.sql
@@ -0,0 +1,4 @@
+CREATE TABLE dup_countries (
+) INHERITS (countries)
+
+REFERENCE: PART1 - Chapter 3 - 3.6. Inheritance
